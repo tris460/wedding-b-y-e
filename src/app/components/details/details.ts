da031c37f827cadb80eb208e8dc6da4b9d18f987
Beatriz Martinez <beatriz.martinez@xaldigital.com>
2025-09-14 12:59:42 -0600

    Add song

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './details.scss'
 })
 export class Details {
+  isPlaying = false;
+  private audio: HTMLAudioElement;
 
+  constructor() {
+    this.audio = new Audio('Sweet Child O Mine.mp3');
+    this.audio.loop = true;
+    this.audio.volume = 0.5;
+    this.audio.currentTime = 8;
+  }
+
+  toggleMusic() {
+    if (this.isPlaying) {
+      this.audio.pause();
+    } else {
+      this.audio.play().catch(error => {
+        console.error('Error playing audio:', error);
+      });
+    }
+    this.isPlaying = !this.isPlaying;
+  }
 }
